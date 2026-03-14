87e1b12bee26eef5befd29b2244a5618c06b8e1d
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-14 23:36:57 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+ 
+
+  return (
+    <>
+      <section id="center">
+        <h1>React World on the go....</h1>
+        <Countries></Countries>
+      </section>
+    </>
+  );
+}
+
+export default App;
